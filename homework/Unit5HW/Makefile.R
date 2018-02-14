f6c7aa4da16bd95505a6ce70c636a3570ce201da
BuildTools <amypaschal@me.com>
2018-02-14 11:54:26 -0700

    Formattng tweaks. Update file name.

--- homework/Unit5HW/Makefile.R
+++ homework/Unit5HW/Makefile.R
@@ -1,3 +1,5 @@
 knitr::knit("apaschal_Livesession5assignment_MS6306_sec402.Rmd")
+markdown::markdownToHTML("apaschal_Livesession5assignment_MS6306_sec402.md",output="apaschal_Livesession5assignment_MS6306_sec402.html")
 knitr::knit("Codebook.Rmd")
+markdown::markdownToHTML("Codebook.md",output="Codebook.html")
 
